e2ef59810af42d13f3122003039412f44cbddc20
navy_glb <navy2013@sina.com>
2022-01-07 14:03:34 +0800

    测试步骤数据驱动

---
+++ 14-UI -步骤驱动/page/main.py
@@ -0,0 +1,17 @@
+#！/usr/bin/env python
+# -*-encoding:utf-8-*-
+# author: navy
+# 2022/1/7 11:23
+
+from selenium.webdriver.common.by import By
+
+from page.base_page import BasePage
+
+
+class Main(BasePage):
+
+    def goto_search(self):
+        # self.find(By.ID, 'com.xueqiu.android:id/tv_search').click()
+        self.steps('../page/main.yml')
+
+
